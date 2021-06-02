415532093ad46b6a6e25092a2bed8376d43b41f3
rikazfawaiz <fawaizrikaz@gmail.com>
2021-06-02 08:25:58 +0700

    'delete loop'

--- app.py
+++ app.py
@@ -3,34 +3,34 @@ import pandas as pd
 
 url = 'https://shopee.co.id/api/v4/search/search_items'
 
-for page in range(1,101):
-    param = {
-        'by' : 'relevancy',
-        'keyword' : 'senter',
-        'limit' : 50,
-        'newest' : 0,
-        'order' : 'desc',
-        'page_type' : 'search',
-        'scenario' : 'PAGE_GLOBAL_SEARCH',
-        'version' : 2,
-        'page': page
+param = {
+    'by' : 'relevancy',
+    'keyword' : 'senter',
+    'limit' : 50,
+    'newest' : 0,
+    'order' : 'desc',
+    'page_type' : 'search',
+    'scenario' : 'PAGE_GLOBAL_SEARCH',
+    'version' : 2,
+    'page': 0
+}
+
+items = requests.get(url,params=param).json()
+products = []
+for item in items['items']:
+    data = {
+        'id_product' : item['item_basic']['itemid'],
+        'name' : item['item_basic']['name'],
+        'stock' : item['item_basic']['stock'],
+        'price' : item['item_basic']['price']
     }
+    products.append(data)
 
-    items = requests.get(url,params=param).json()
-    products = []
-    for item in items['items']:
-        data = {
-            'id_product' : item['item_basic']['itemid'],
-            'name' : item['item_basic']['name'],
-            'stock' : item['item_basic']['stock'],
-            'price' : item['item_basic']['price']
-        }
-        products.append(data)
+df = pd.DataFrame.from_dict(products)
+df.to_csv('products.csv',mode='w',index=False)
 
-    df = pd.DataFrame.from_dict(products)
-    df.to_csv('products.csv',mode='a',index=False)
-    # print(df)
-    print('page :',page)
+print(len(df['id_product'].unique()))
+
 
 
 
